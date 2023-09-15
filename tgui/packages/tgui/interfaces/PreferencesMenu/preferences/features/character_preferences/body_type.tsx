ebe72b78f82ac0c390a8b1fdff9798f639c5a63a
Bob <bobjogaiscoding@gmail.com>
2023-09-15 10:02:28 -0300

    fart

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/body_type.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/body_type.tsx
@@ -1,10 +1,6 @@
-import { FeatureChoiced, FeatureChoicedServerData, FeatureDropdownInput, FeatureValueProps } from '../base';
+import { FeatureChoiced, FeatureDropdownInput } from '../base';
 
 export const body_type: FeatureChoiced = {
   name: 'Body type',
-  component: (
-    props: FeatureValueProps<string, string, FeatureChoicedServerData>
-  ) => {
-    return <FeatureDropdownInput buttons {...props} />;
-  },
+  component: FeatureDropdownInput,
 };
